b1c0c48cce5aa8ea2f1092c90c8ecd53b793cd01
BehzadJanNesar <behzadjannesar.2001@gmail.com>
2024-02-02 19:58:06 +0330

    commit all files

---
+++ app.js
@@ -0,0 +1,54 @@
+const input = document.querySelectorAll("#input");
+let btn = document.querySelector("#btn");
+let close = document.querySelector("#close");
+let succsesPm = document.querySelector("#succses-pm");
+
+input.forEach((element, index1) => {
+   let nextElement = element.nextElementSibling;
+   let prevElement = element.previousElementSibling;
+   element.addEventListener("keyup", (e) => {
+      if (element.value.length > 1) {
+         element.value = "";
+         return;
+      }
+
+      if (
+         nextElement &&
+         nextElement.hasAttribute("disabled") &&
+         element.value !== ""
+      ) {
+         nextElement.removeAttribute("disabled");
+         nextElement.focus();
+      }
+
+      if (e.key === "Backspace") {
+         input.forEach((el, index2) => {
+            if (index1 <= index2 && prevElement) {
+               el.setAttribute("disabled", true);
+               element.value = "";
+               prevElement.focus();
+            }
+         });
+      }
+      if (!input[3].disabled && input[3].value !== "") {
+         btn.classList.add("active");
+         btn.removeAttribute("disabled");
+         return;
+      }
+      btn.setAttribute("disabled", true);
+      btn.classList.remove("active");
+      succsesPm.classList.add("hide");
+   });
+});
+btn.addEventListener("click", () => {
+   succsesPm.classList.remove("hide");
+   succsesPm.classList.add("show");
+   setTimeout(() => {
+      succsesPm.classList.add("hide");
+   }, 5000);
+});
+close.addEventListener("click", () => {
+   succsesPm.classList.add("hide");
+});
+
+window.addEventListener("load", () => input[0].focus());
